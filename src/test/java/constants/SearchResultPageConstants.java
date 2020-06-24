c275851996311596890bb342acb9568e19aba24a
Igor Gindin <igindin@gmail.com>
2020-06-24 07:08:56 +0300

    search result page

--- src/test/java/constants/SearchResultPageConstants.java
+++ src/test/java/constants/SearchResultPageConstants.java
@@ -9,8 +9,22 @@ public class SearchResultPageConstants {
     // == String ==
     public static final String STR_SEARCH_RESULT_PANEL = "changeView";
     public static final String STR_SORTING_SELECTOR = "sortOptions1";
-    public static final String STR_CHEAPEST_PRODUCT = "//*[@id='mainProductGrid']/li[1]";
+    public static final String STR_PRODUCTS = "//*[@id='mainProductGrid']/li";
+    public static final String STR_CHEAPEST_PRODUCT = STR_PRODUCTS + "[1]";
+    public static final String STR_PRICE = "//*[@class='number']";
+    public static final String STR_ADD_PRODUCT = "//button[contains(@class, 'js-add-to-cart')]";
+    public static final String STR_CART_CONTAINER = "//*[@id='cartMiddleContent']";
+    public static final String STR_CART_SINGLE_PRODUCT = "//*[contains(@class, 'miglog-cart-summary-prod-wrp')]";
+    public static final String STR_PRICE_FROM_CART = STR_CART_SINGLE_PRODUCT + "//*[contains(@class, 'miglog-prod-totalPrize')]/span[1]";
+    public static final String STR_POSTAGE_PRICE = "infoSubText";
+    public static final String STR_TOTAL_PRICE = "//*[@class='btnToggleCart']//*[@class='currency']";
 
     // == By =
     public static final By BY_SEARCH_RESULT_PANEL = By.id(STR_SEARCH_RESULT_PANEL);
+    public static final By BY_CART_SINGLE_PRODUCT = By.xpath(STR_CART_SINGLE_PRODUCT);
+
+    // == sub-element ==
+    public static final By BY_PRICE_SUB_ELEMENT_PRODUCT_CARD = By.xpath("." + STR_PRICE);
+    public static final By BY_ADD_PRODUCT_SUB_ELEMENT_PRODUCT_CARD = By.xpath("." + STR_ADD_PRODUCT);
+    public static final By BY_PRICE_FROM_CARD_SUB_ELEMENT_SINGLE_PRODUCT = By.xpath("." + STR_PRICE_FROM_CART);
 }
--- src/test/java/pom/SearchResultPage.java
+++ src/test/java/pom/SearchResultPage.java
@@ -1,13 +1,18 @@
 package pom;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+
 import static constants.SearchResultPageConstants.*;
 import static utilitues.Waits.fluentWaitElementClickable;
+import static utilitues.Waits.fluentWaitElementExists;
 
 public class SearchResultPage {
 
@@ -25,13 +30,29 @@ public class SearchResultPage {
     private WebElement searchResultPanel;
     @FindBy(id = STR_SORTING_SELECTOR)
     private Select slctSort;
+    @FindBy(xpath = STR_PRODUCTS)
+    private List<WebElement> products;
     @FindBy(xpath = STR_CHEAPEST_PRODUCT)
     private WebElement cheapestProduct;
+    @FindBy(xpath = STR_CART_SINGLE_PRODUCT)
+    private List<WebElement> productsInCart;
+    @FindBy(className = STR_POSTAGE_PRICE)
+    private WebElement postagePrice;
+    @FindBy(xpath = STR_TOTAL_PRICE)
+    private WebElement totalPrice;
 
     // == getters ==
 
     public WebElement getSearchResultPanel() {
         return searchResultPanel;
+    }
+
+    public List<WebElement> getProducts() {
+        return products;
+    }
+
+    public WebElement getCheapestProduct() {
+        return cheapestProduct;
     }
 
     public Select getSlctSort() {
@@ -44,5 +65,60 @@ public class SearchResultPage {
         slctSort.selectByIndex(0);
 
         fluentWaitElementClickable(driver, BY_SEARCH_RESULT_PANEL, 5);
+
+        PageFactory.initElements(driver, this);
     }
+
+    public double getPrice(WebElement card) {
+
+        String price = card.findElement(BY_PRICE_SUB_ELEMENT_PRODUCT_CARD).getText();
+
+        return Double.parseDouble(price);
+    }
+
+    public double getCheapestPrice() {
+
+        return getPrice(cheapestProduct);
+    }
+
+    public void moveMouseToCard(WebElement card) {
+        Actions builder = new Actions(driver);
+        builder.moveToElement(card).build().perform();
+    }
+
+    public void moveMouseToCheapestCard() {
+        moveMouseToCard(cheapestProduct);
+    }
+
+    public void addProductToCart(WebElement card) {
+        moveMouseToCard(card);
+        card.findElement(BY_ADD_PRODUCT_SUB_ELEMENT_PRODUCT_CARD).click();
+
+        fluentWaitElementExists(driver, BY_CART_SINGLE_PRODUCT, 5);
+
+        PageFactory.initElements(driver, this);
+    }
+
+    public void addCheapestProductToCart() {
+        addProductToCart(cheapestProduct);
+    }
+
+    public double getPriceFirstProductInCart() {
+        String price = productsInCart.get(0).findElement(BY_PRICE_FROM_CARD_SUB_ELEMENT_SINGLE_PRODUCT).getText();
+
+        return Double.parseDouble(price);
+    }
+
+    public double getPostagePrice() {
+        String price = postagePrice.getText();
+
+        return Double.parseDouble(price);
+    }
+    
+    public double getTotalPrice() {
+        String price = totalPrice.getText();
+
+        return Double.parseDouble(price);
+    }
+
 }
